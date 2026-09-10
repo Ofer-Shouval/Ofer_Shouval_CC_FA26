bea76800668d62605c5891984eb5ec5cf09646c2
Ofer-Shouval <oshouval@pratt.edu>
2026-09-10 15:50:44 -0400

    week 2 demos

---
+++ Section F/week2/sketch1.js
@@ -0,0 +1,21 @@
+function setup(){
+    createCanvas(windowWidth,windowHeight)
+    background(100)
+
+    fill(100,0,0)
+    // noStroke()
+    strokeWeight(15)
+    stroke(200,100,0)
+    
+
+}
+function draw(){
+ellipse(mouseX,mouseY,50, 50)
+
+}
+
+function mousePressed(){
+        background(100)
+
+
+}
